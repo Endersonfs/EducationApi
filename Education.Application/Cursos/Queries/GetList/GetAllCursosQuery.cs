46ebcd673adcd8ac02c61ac989c7ba7f2ce4fad5
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2023-02-15 14:57:11 -0400

    Resolviendo cambios CQRS

---
+++ Education.Application/Cursos/Queries/GetList/GetAllCursosQuery.cs
@@ -0,0 +1,30 @@
+﻿using Education.Domain;
+using Education.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Application.Cursos.Queries.GetList
+{
+    public class GetAllCursosQuery
+    {
+        public class GetAllCursosQueryRequest : IRequest<List<Curso>> { }
+        public class GetAllCursosQueryHandle : IRequestHandler<GetAllCursosQueryRequest, List<Curso>>
+        {
+            private readonly EducationDbContext _context;
+            public GetAllCursosQueryHandle(EducationDbContext context)
+            {
+                    _context= context;
+            }
+            public async Task<List<Curso>> Handle(GetAllCursosQueryRequest request, CancellationToken cancellationToken)
+            {
+                var cursos = await _context.Cursos.ToListAsync();
+                return cursos;
+            }
+        }
+    }
+}
